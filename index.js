9fa42d6720ffa8fd82744fc8cc2c5ef060f83226
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-02-27 02:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
-function destructivelyAppendKitten (kittens){
-  destructivelyAppendKitten.push()
-};
+destructivelyAppendKitten.push()
